e0a7fec71efb59c1e3b804ee038a1648389caa70
Bruno Henrique <bhsantos16@gmail.com>
2024-07-12 23:45:38 -0300

    comitando codigo python

---
+++ LivroCap1.py
@@ -0,0 +1,23 @@
+def pesquisa(lista, item):
+    baixo = 0
+    alto = len(lista) - 1
+
+    while baixo <= alto:
+        meio = (baixo + alto) // 2
+        chute = lista[meio]
+        if chute == item:
+            print("Achou!")
+            return meio
+        if chute > item:
+            print("Esta alto")
+            alto = meio - 1
+        else:
+            print("Esta baixo")
+            baixo = meio + 1
+    return None
+
+minha_lista = [1,3,5,7,9,11]
+print(pesquisa(minha_lista, 7))
+
+
+
